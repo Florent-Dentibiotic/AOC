a626ad62092f3a285137eb60a48139cdb28e26c2
MacbookAir <florent2303@pm.me>
2023-11-27 09:55:27 +0100

    update : advent year model

---
+++ Advent Model/Day_1/aoc.js
@@ -0,0 +1,6 @@
+/**
+ * AOC Day 1
+ */
+const input = document.querySelector('pre').textContent
+const response_1 = document.querySelector('#response_1')
+const response_2 = document.querySelector('#response_2')
---
+++ Advent Model/Day_18/aoc.js
@@ -0,0 +1,6 @@
+/**
+ * AOC Day 18
+ */
+const input = document.querySelector('pre').textContent
+const response_1 = document.querySelector('#response_1')
+const response_2 = document.querySelector('#response_2')
