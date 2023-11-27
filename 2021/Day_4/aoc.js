962fab2c743b170c69dd4e21b11d4c05531f377c
MacbookAir <florent2303@pm.me>
2023-11-27 09:40:57 +0100

    update : 2023 structure

---
+++ 2021/Day_4/aoc.js
@@ -0,0 +1,6 @@
+/**
+ * AOC Day 1
+ */
+const input = document.querySelector('pre').textContent
+const bingos = input.split('\n\n')
+console.log(bingos)
--- 2022/Day_1/aoc.js
+++ 2022/Day_1/aoc.js
@@ -2,6 +2,7 @@
  * AOC Day 1
  */
 const input = document.querySelector('pre').textContent
+const response_2 = document.querySelector('#response_2')
 const elves = input.split('\n\n')
 const each_elves = elves.map((elve) =>
   elve
@@ -13,10 +14,16 @@ const each_elves = elves.map((elve) =>
     )
 )
 const max_calories = Math.max(...each_elves)
+
+response_1.textContent = `${max_calories}`
+
 const sort_elves = each_elves.sort(function compareNumbers(a, b) {
   return a - b
 })
+
 const top_three =
   sort_elves[sort_elves.length - 1] +
   sort_elves[sort_elves.length - 2] +
   sort_elves[sort_elves.length - 3]
+
+response_2.textContent = `${top_three}`
