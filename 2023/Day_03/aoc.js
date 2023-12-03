0982aeb67f33d0a7868ef97389765fe902cc9bc1
MacbookAir <florent2303@pm.me>
2023-12-03 10:29:39 +0100

    update day_3 part 1

--- 2023/Day_03/aoc.js
+++ 2023/Day_03/aoc.js
@@ -4,3 +4,69 @@
 const input = document.querySelector('pre').textContent
 const response_1 = document.querySelector('#response_1')
 const response_2 = document.querySelector('#response_2')
+
+let symbols_coordinates = []
+const standard_caracteres = new RegExp('[0-9]|\\.')
+const number_regex = new RegExp('[0-9]+', 'g')
+
+let input_lines = input.split('\n')
+
+for (let i = 0; i < input_lines.length; i++) {
+  for (let j = 0; j < input_lines[i].length; j++) {
+    standard_caracteres.test(input_lines[i][j])
+      ? null
+      : symbols_coordinates.push(
+          `${i - 1},${j + 1}`,
+          `${i - 1},${j}`,
+          `${i - 1},${j - 1}`,
+          `${i},${j - 1}`,
+          `${i},${j}`,
+          `${i},${j + 1}`,
+          `${i + 1},${j + 1}`,
+          `${i + 1},${j}`,
+          `${i + 1},${j - 1}`
+        )
+  }
+}
+
+const isValid = (word_x, word_y, word_length) => {
+  let coordinates = []
+  for (let i = 0; i < word_length; i++) {
+    coordinates.push(`${word_x},${word_y + i}`)
+  }
+  return coordinates.some((coordinate) => symbols_coordinates.includes(coordinate))
+}
+
+const isNumbersValid = (line, line_index, numbers, number_index, sum) => {
+  if (number_index == numbers.length) return sum
+  console.log(line, numbers, number_index, sum)
+  let total = sum
+  isValid(
+    line_index,
+    line.indexOf(numbers[number_index]),
+    numbers[number_index].length
+  ) && (total += parseInt(numbers[number_index]))
+
+  return isNumbersValid(
+    line.replace(numbers[number_index], '.'.repeat(numbers[number_index].length)),
+    line_index,
+    numbers,
+    number_index + 1,
+    total
+  )
+}
+
+let total = 0
+
+for (let i = 0; i < input_lines.length; i++) {
+  let number_in_row = input_lines[i].match(number_regex)
+  //console.log(number_in_row)
+  let row_sum = isNumbersValid(input_lines[i], i, number_in_row, 0, 0)
+  //console.log(row_sum)
+  total += row_sum
+}
+
+response_1.textContent = total
+
+//FIRST TRY : 539741
+//SECOND TRY : 539257
