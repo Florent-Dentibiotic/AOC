646f0791b391dc1721bc73a7e595f364f67afab0
MacbookAir <florent2303@pm.me>
2023-12-03 13:42:09 +0100

    update Day_3 part_2 Done !

--- 2023/Day_03/aoc.js
+++ 2023/Day_03/aoc.js
@@ -39,7 +39,7 @@ const isValid = (word_x, word_y, word_length) => {
 
 const isNumbersValid = (line, line_index, numbers, number_index, sum) => {
   if (number_index == numbers.length) return sum
-  console.log(line, numbers, number_index, sum)
+
   let total = sum
   isValid(
     line_index,
@@ -60,9 +60,9 @@ let total = 0
 
 for (let i = 0; i < input_lines.length; i++) {
   let number_in_row = input_lines[i].match(number_regex)
-  //console.log(number_in_row)
+
   let row_sum = isNumbersValid(input_lines[i], i, number_in_row, 0, 0)
-  //console.log(row_sum)
+
   total += row_sum
 }
 
@@ -70,3 +70,63 @@ response_1.textContent = total
 
 //FIRST TRY : 539741
 //SECOND TRY : 539257
+
+// PART 2
+
+let stars_coordinates = {}
+
+for (let i = 0; i < input_lines.length; i++) {
+  for (let j = 0; j < input_lines[i].length; j++) {
+    input_lines[i][j] === '*' &&
+      (stars_coordinates = { ...stars_coordinates, [`${i},${j}`]: [] })
+  }
+}
+
+const isNextToAStar = (number_x, number_y, number_length) => {
+  let coordinates = []
+  for (let i = 0; i < number_length; i++) {
+    coordinates.push(
+      `${number_x},${number_y + i - 1}`,
+      `${number_x},${number_y + i}`,
+      `${number_x},${number_y + i + 1}`,
+      `${number_x - 1},${number_y + i - 1}`,
+      `${number_x - 1},${number_y + i}`,
+      `${number_x - 1},${number_y + i + 1}`,
+      `${number_x + 1},${number_y + i - 1}`,
+      `${number_x + 1},${number_y + i}`,
+      `${number_x + 1},${number_y + i + 1}`
+    )
+  }
+  return Object.keys(stars_coordinates).find((position) => coordinates.includes(position))
+}
+
+const numberCoordinates = (line, line_index, numbers, number_index) => {
+  if (number_index == numbers.length) return
+  const star = isNextToAStar(
+    line_index,
+    line.indexOf(numbers[number_index]),
+    numbers[number_index].length
+  )
+  if (star)
+    stars_coordinates = {
+      ...stars_coordinates,
+      [star]: [...stars_coordinates[star], numbers[number_index]],
+    }
+  return numberCoordinates(
+    line.replace(numbers[number_index], '.'.repeat(numbers[number_index].length)),
+    line_index,
+    numbers,
+    number_index + 1
+  )
+}
+
+for (let i = 0; i < input_lines.length; i++) {
+  let number_in_row = input_lines[i].match(number_regex)
+  numberCoordinates(input_lines[i], i, number_in_row, 0)
+}
+
+let GEARS = Object.values(stars_coordinates).reduce((acc, gear) => {
+  return acc + (gear.length > 1 ? gear[0] * gear[1] : 0)
+}, 0)
+
+response_2.textContent = GEARS
