bc46ecd3e6841b351bccd9aa64b61ac0bef9a8f4
MacbookAir <florent2303@pm.me>
2023-12-04 09:17:25 +0100

    update : Day_4 Done !

--- 2023/Day_04/part_1/aoc.js
+++ 2023/Day_04/part_1/aoc.js
@@ -1,5 +1,17 @@
 /**
- * AOC Day 1
+ * AOC Day 4 Part_1
  */
 const input = document.querySelector('pre').textContent
 const response = document.querySelector('#response')
+const games = input.split('\n')
+const number_regex = new RegExp('[0-9]+', 'g')
+
+const solution = games.reduce((acc, game) => {
+  const all_num = game.match(number_regex).splice(1)
+  const unique = new Set(all_num).size
+  let repeated = all_num.length - unique
+  const current = repeated > 0 ? 1 * Math.pow(2, repeated - 1) : 0
+  return acc + current
+}, 0)
+
+response.textContent = solution
--- 2023/Day_04/part_2/aoc.js
+++ 2023/Day_04/part_2/aoc.js
@@ -1,5 +1,30 @@
 /**
- * AOC Day 1
+ * AOC Day 4 Part_2
  */
 const input = document.querySelector('pre').textContent
 const response = document.querySelector('#response')
+const games = input.split('\n')
+const number_regex = new RegExp('[0-9]+', 'g')
+
+let winning_copies = []
+
+games.map((game) => {
+  const all_num = game.match(number_regex).splice(1)
+  const unique = new Set(all_num).size
+  let repeated = all_num.length - unique
+  winning_copies.push(repeated)
+})
+
+let scratchcards = []
+
+for (let i = 0; i < games.length; i++) {
+  scratchcards.push(1)
+}
+
+winning_copies.map((game, index) => {
+  for (let i = 0; i < game; i++) {
+    scratchcards[i + index + 1] += 1 * scratchcards[index]
+  }
+})
+
+response.textContent = scratchcards.reduce((acc, cur) => acc + cur, 0)
