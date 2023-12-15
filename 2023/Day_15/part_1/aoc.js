180196191a912903b466911bbfb7e8b3419c87b9
MacbookAir <florent2303@pm.me>
2023-12-15 14:58:36 +0100

    update : day 15 part _1 Done

--- 2023/Day_15/part_1/aoc.js
+++ 2023/Day_15/part_1/aoc.js
@@ -1,5 +1,20 @@
 /**
  * AOC Day 1
  */
-const input = document.querySelector('pre').textContent
+const input = document.querySelector('#input').textContent
+const exemple = document.querySelector('#exemple').textContent
 const response = document.querySelector('#response')
+
+const entries = input.split(',')
+
+const define_result = (sequence, current_value, index) => {
+  current_value += sequence.charCodeAt(index)
+  current_value *= 17
+  current_value = current_value % 256
+  if (index + 1 === sequence.length) return current_value
+  return define_result(sequence, current_value, index + 1)
+}
+
+const results = entries.map((string) => define_result(string, 0, 0))
+
+response.textContent = results.reduce((acc, cur) => acc + cur, 0)
